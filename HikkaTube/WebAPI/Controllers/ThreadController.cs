87738c5ad438f8313adce90c6e5eba85ffedc53f
Uladzislau Shved <Uladzislau_Shved@epam.com>
2015-12-17 17:42:43 +0300

    add thread view

---
+++ HikkaTube/WebAPI/Controllers/ThreadController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataSource;
+
+namespace WebAPI.Controllers
+{
+  public class ThreadController : Controller
+  {
+    private readonly IRepository<Thread> _threadRepository;
+
+    public ThreadController(IRepository<Thread> threadRepository)
+    {
+      _threadRepository = threadRepository;
+    }
+
+    public ActionResult Index(Guid? id)
+    {
+      if (id == null)
+      {
+        return View("ErrorPage", "Board");
+      }
+
+      var thread = _threadRepository.GetById(id.Value);
+
+      return View(thread);
+    }
+  }
+}
